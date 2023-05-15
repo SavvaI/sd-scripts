d75a2937d59c3c51f5e9bef9e46de734aa466e54
SavvaI <savvaignatiev@gmail.com>
2023-05-15 09:22:19 +0000

    Update DnD config

--- train_db_dnd.sh
+++ train_db_dnd.sh
@@ -6,17 +6,17 @@ python train_db.py \
     --output_dir=./checkpoints/dreambooth_dnd \
     --output_name=dnd \
     --save_model_as=ckpt \
-    --max_train_steps=100000 \
+    --max_train_steps=50000 \
     --sample_every_n_steps=300 \
     --learning_rate=2e-5 \
     --optimizer_type="AdamW8bit" \
     --xformers \
     --mixed_precision="fp16" \
-    --train_batch_size=100 \
+    --train_batch_size=150 \
     --cache_latents \
     --gradient_checkpointing \
     --save_precision='fp16' \
     --sample_prompts=./conf/sample_prompt_dnd.txt \
-    --vae_batch_size=150
+    --vae_batch_size=300
     --prior_loss_weight=0.0 \
-    --stop_text_encoder_training=0
+    --stop_text_encoder_training=-1
