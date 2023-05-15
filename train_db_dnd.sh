fd9368c62e37137d7025aaa60eb08b0bf006ace1
SavvaI <savvaignatiev@gmail.com>
2023-05-15 10:24:52 +0000

    Correct config typo

--- train_db_dnd.sh
+++ train_db_dnd.sh
@@ -12,11 +12,11 @@ python train_db.py \
     --optimizer_type="AdamW8bit" \
     --xformers \
     --mixed_precision="fp16" \
-    --train_batch_size=150 \
+    --train_batch_size=125 \
     --cache_latents \
     --gradient_checkpointing \
     --save_precision='fp16' \
     --sample_prompts=./conf/sample_prompt_dnd.txt \
-    --vae_batch_size=300
+    --vae_batch_size=300 \
     --prior_loss_weight=0.0 \
     --stop_text_encoder_training=-1
