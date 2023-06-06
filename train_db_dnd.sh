78ece72de59a954cbdbbe331eb61f8dc1aadf642
SavvaI <savvaignatiev@gmail.com>
2023-06-06 16:03:05 +0300

    Add command line arguments to the script (experiment name, learning rate, number iterations to freeze encoder after)

--- train_db_dnd.sh
+++ train_db_dnd.sh
@@ -1,14 +1,22 @@
 set -e
+EXPERIMENT_NAME=$1
+LEARNING_RATE=$2
+STOP_ENCODER=$3
+
+echo Experiment name: $EXPERIMENT_NAME
+echo Learning rate: $LEARNING_RATE -  Recommended learning rate 1e-5
+echo Number of iterations to keep text encoder unfreezed in the beginning: $STOP_ENCODER -  Recommended number -1
+
 source /opt/anaconda3/bin/activate kohya-ss
 python train_db.py \
     --pretrained_model_name_or_path=./checkpoints/model.ckpt \
     --dataset_config=./conf/dreambooth_dnd.toml \
-    --output_dir=./checkpoints/dreambooth_dnd \
+    --output_dir="./checkpoints/dreambooth_dnd${EXPERIMENT_NAME}" \
     --output_name=dnd \
     --save_model_as=ckpt \
     --max_train_steps=50000 \
     --sample_every_n_steps=300 \
-    --learning_rate=2e-5 \
+    --learning_rate=$LEARNING_RATE \
     --optimizer_type="AdamW8bit" \
     --xformers \
     --mixed_precision="fp16" \
@@ -17,6 +25,6 @@ python train_db.py \
     --gradient_checkpointing \
     --save_precision='fp16' \
     --sample_prompts=./conf/sample_prompt_dnd.txt \
-    --vae_batch_size=300 \
+    --vae_batch_size=100 \
     --prior_loss_weight=0.0 \
-    --stop_text_encoder_training=-1
+    --stop_text_encoder_training=$STOP_ENCODER
